0d959c792936c88995c4d6e13d2bbcab920c6e79
Alice Sousa <50872648+AliceSousa063@users.noreply.github.com>
2020-09-02 11:08:53 -0300

    Update GET_Alice.py

--- Metodos_GET/GET_Alice.py
+++ Metodos_GET/GET_Alice.py
@@ -7,18 +7,8 @@ sys.path.insert(0, '../Funcoes')
 # Funcoes
 from Funcoes.Alice import Alice
 f_alice = Alice()
-
-class metodo_get_alice(Resource):
-    def get(self):
-        try:
-            result = f_alice.funcao_alice()
-        except:
-            result = []
-        finally:
-            return jsonify(result)class
-
         
-class municipio_estado(Resource):
+class Municipio_estado(Resource):
     def get(self, tipo, sigla):
         try:
             result = f_alice.municipio_estado(tipo, sigla)
@@ -28,7 +18,7 @@ class municipio_estado(Resource):
             return jsonify(result) 
         
         
-class municipio_estado_datas(Resource):
+class Municipio_estado_datas(Resource):
     def get(self, tipo, sigla, data_inicio, data_fim):
         try:
             result = f_alice.municipio_estado_datas(tipo, sigla, data_inicio, data_fim)
